a795a2937db96845af022bfe624abc4a337b4e41
Smit <aquarocker20@gmail.com>
2022-02-11 13:27:02 +0530

    added: analytics and link to blogs, package updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     image: `src/images/social.png`,
     siteUrl: `https://devsmit.in/`,
     author: `@devsmitin`,
-    twitterUsername: `@devsmitin`
+    twitterUsername: `@devsmitin`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -32,15 +32,33 @@ module.exports = {
     },
     `gatsby-plugin-postcss`,
     `gatsby-source-gravatar`,
-    // {
-    //   resolve: `gatsby-source-gravatar`,
-    //   options: {
-    //     emails: [
-    //       `first@example.com`,
-    //     ],
-    //     query: `?size=200&m=dp`,
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-EC1PWSL2KS", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        // gtagConfig: {
+        //   optimize_id: "OPT_CONTAINER_ID",
+        //   anonymize_ip: true,
+        //   cookie_expires: 0,
+        // },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          // exclude: ["/preview/**", "/do-not-track/me/too/"],
+          // Defaults to https://www.googletagmanager.com
+          // origin: "YOUR_SELF_HOSTED_ORIGIN",
+        },
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
